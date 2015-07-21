1560275df8225f7c57825d00371a448c5552c472
Ben <ben.c.hawker@gmail.com>
2015-07-21 18:09:19 +0100

    Adding Day 1 Boris Bike Work

---
+++ lib/docking_station.rb
@@ -0,0 +1,8 @@
+class DockingStation
+
+  def release_bike
+  	bike = Bike.new
+  end
+
+
+end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,12 @@
+require "docking_station"
+
+describe DockingStation do
+  it {is_expected.to respond_to :release_bike}
+
+ it "releases a working bike" do 
+ 	instance = Bike.new
+ 	expect(instance).to be_working
+ end
+
+end
+
